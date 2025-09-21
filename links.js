08997d2f3ea67f5ed1c7b4af8d28ae13bec7879a
TexableMonk <rowergame1@gmail.com>
2025-09-21 20:12:23 +0200

    Create links.js

---
+++ links.js
@@ -0,0 +1,9 @@
+export const rawConfigLines1 = [
+  "(Spiderman universum)[https://vider.info/vid/+fx5nxvx]",
+  "(Spiderman homecoming)[https://vider.info/vid/+fncxx8e-]",
+];
+
+export const rawConfigLines2 = [
+  "(Serial Kiepscy)[https://www.swiatwedlugkiepskich.pl/]",
+  "(Świat według Bundych)[https://vider.info/dir/+dxxecx]",
+];
